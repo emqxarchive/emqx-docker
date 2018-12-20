34f218e96733dd9502f14019f88b5f0facd852be
zhanghongtong <zhanghongtong@foxmail.com>
2018-12-20 17:32:10 +0800

    rename docker save

--- docker.sh
+++ docker.sh
@@ -112,8 +112,8 @@ docker_save() {
       echo "DOCKER TEST: FAILED - Docker no search images"
       exit 1
     fi
-    docker save ${TARGET}:${BUILD_VERSION}-${ARCH} > emqx-${BUILD_VERSION}-${ARCH}
-    zip -r -m emqx-${BUILD_VERSION}-${ARCH}.zip emqx-${BUILD_VERSION}-${ARCH} 
+    docker save ${TARGET}:${BUILD_VERSION}-${ARCH} > emqx-docker-${BUILD_VERSION}-${ARCH}
+    zip -r -m emqx-docker-${BUILD_VERSION}-${ARCH}.zip emqx-docker-${BUILD_VERSION}-${ARCH} 
 }
 
 docker_push() {
