4997935608cfc6c98ec5ea2e4c2b50c635ebfadd
zhanghongtong <zhanghongtong@foxmail.com>
2019-07-10 16:13:00 +0800

    Update README

--- v3.2/docker-entrypoint.sh
+++ v3.2/docker-entrypoint.sh
@@ -34,6 +34,7 @@ fi
 
 if [[ -z "$EMQX_HOST" ]]; then
     if [[ "$EMQX_CLUSTER__K8S__ADDRESS_TYPE" == "dns" ]] && [[ ! -z "$EMQX_CLUSTER__K8S__NAMESPACE" ]];then
+        # DNSAddress=$(sed -n "/^${LOCAL_IP}/"p /etc/hosts | grep -e "$(hostname).*.${EMQX_CLUSTER__K8S__NAMESPACE}.svc.cluster.local" -o)
         DNSAddress="${LOCAL_IP//./-}.${EMQX_CLUSTER__K8S__NAMESPACE}.pod.cluster.local"
         export EMQX_HOST="$DNSAddress"
     else
